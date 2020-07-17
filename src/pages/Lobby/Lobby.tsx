b2b43c333ee81333e31221fbc9c09c8acfdf20f7
Sarah Berger <57356343+mtsarah@users.noreply.github.com>
2020-07-17 23:20:11 +0200

    feat: implement websocket (#10)
    * feat: implement-websocket
    
    * make websocket connection
    
    * implement message sending
    
    * use correct greeting

--- src/pages/Lobby/Lobby.tsx
+++ src/pages/Lobby/Lobby.tsx
@@ -1,14 +1,69 @@
-import React from "react";
+import React, {
+  useState,
+  useEffect,
+  createRef,
+  FormEvent,
+  useRef,
+  MutableRefObject
+} from "react";
 import { withRouter } from "react-router";
 import { RouteComponentProps } from "react-router-dom";
 
 import styles from "./Lobby.module.scss";
 
 const Lobby = (props: RouteComponentProps): JSX.Element => {
+  const ws: MutableRefObject<WebSocket | undefined> = useRef();
+  useEffect(() => {
+    ws.current = new WebSocket("ws://localhost:8668/ws");
+    ws.current.onopen = () => {
+      console.log("connected");
+      ws.current && ws.current.send("henlo werewolves");
+    };
+
+    ws.current.onmessage = event => {
+      console.log(event.data);
+    };
+
+    ws.current.onclose = () => {
+      console.log("disconnected");
+    };
+  }, []);
+
+  const inputRef = createRef<HTMLInputElement>();
+
+  const [message, setMessage] = useState("");
+
+  const sendMessage = (event: FormEvent) => {
+    event.preventDefault();
+
+    if (inputRef.current && inputRef.current.value !== "") {
+      ws.current && ws.current.send(inputRef.current.value);
+    }
+  };
+
   return (
-    <>
-      <div className={styles.choose}>Hello werewolves</div>
-    </>
+    <main>
+      <form onSubmit={sendMessage}>
+        <div className={styles.choose}>Henlo werewolves</div>
+        <ul>
+          <li>
+            <label htmlFor="message">Message</label>
+            <input
+              id="message"
+              ref={inputRef}
+              type="text"
+              value={message}
+              onChange={() => {
+                setMessage(inputRef.current ? inputRef.current.value : "");
+              }}
+            />
+          </li>
+          <li>
+            <button type="submit">Send</button>
+          </li>
+        </ul>
+      </form>
+    </main>
   );
 };
 
