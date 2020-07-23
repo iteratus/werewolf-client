7f070cae6e3233fc64ae12ea0ea7ef320d581448
rohrlaf <rohrlaf@mailbox.org>
2020-07-23 22:03:09 +0200

    Add first actions

---
+++ ruleset/actions.ts
@@ -0,0 +1,29 @@
+export const voteToLynch = (): string => {
+  // render list of other players
+  // take vote
+  const voteOnPlayer = null;
+  return voteOnPlayer;
+};
+
+export const doubleVoteToLynch = (): string[] => {
+  // render list of other players
+  // take double vote
+  const voteOnPlayer = [null, null];
+  return voteOnPlayer;
+};
+
+export const randomVoteToLynch = (): string => {
+  return Math.random() ? "" : null;
+};
+
+export const voteToKill = (): string => {
+  const voteOnPlayer = null;
+  return voteOnPlayer;
+};
+
+export const chooseCouple = () => {
+  // render list of all players
+  // choose two players
+  // set couple death action on player 1
+  // set couple death action on player all players2
+};
