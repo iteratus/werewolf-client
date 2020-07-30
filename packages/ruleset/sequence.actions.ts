3be0f0d15d2e743a546ca6796968ec8af36c5c0f
rohrlaf <rohrlaf@mailbox.org>
2020-07-30 18:54:11 +0200

    Add phases and actions

---
+++ packages/ruleset/sequence.actions.ts
@@ -0,0 +1,22 @@
+export const special = () => {
+  // use special skills
+  // example: Amor defines couple
+};
+
+export const kill = (): string[] => {
+  // killers vote on victim
+  const victims = [null];
+  return victims;
+};
+
+export const reveal = (): string[] => {
+  // reveal identities
+  const players = [null];
+  return players;
+};
+
+export const lynch = (): string[] => {
+  // reveal identities
+  const players = [null];
+  return players;
+};
